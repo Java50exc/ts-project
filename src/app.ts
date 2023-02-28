d3754fd5c3006bd841b36c4998b50f6760abe8e6
Yuragranovsky <yuragranovsky@gmail.com>
2023-02-28 19:00:21 +0200

    shift / unshift round (cipher / decipher)

--- src/app.ts
+++ src/app.ts
@@ -1,47 +1,6 @@
-// for(let i = 0; i < 3; i++) {
-//     setTimeout(()=> console.log(i))
-// }
-let num: number;
-num = 12;
-let str: string = "abcd";
-str = "lmn";
-let color: "red"|"blue"|"green";
-color = "red";
-function f1(n1: number, n2: number): number {
-    return n1 + n2;
-}
-//f1(1, "1");
-function f2(array: number[]): number {
-    return array.reduce((res, cur)=> res + cur);
-}
-function stringNumberAction(strNum: string|number): number {
-    let res:number;
-    if(typeof strNum == "string"){
-        res = strNum.length;
-    } else {
-        res = strNum * 2;
-    }
-    return res;
-}
-console.log(stringNumberAction("10"));
-let ar: number[] | string[];
-ar = [1, 2, 3];
-ar = ["1", "avc", "3"];
-let ar1: (number|string)[];
-ar1 = [1, "a"];
-type Color = "red"|"blue"|"green"|"gray"|"yellow";
-let someColor: Color;
-type Comparator =( (num1: number, num2: number) => number) | ((ar:number[])=> number);
-let someComparator: Comparator;
-someComparator = (n1, n2) => n1 - n2;
-someComparator = f1;
-someComparator = f2;
-type Person = {
-    id: number;
-    name: string;
-    age?: number;
-}
-const person:Person = {id: 123, name: "Vasya"}
+const aCodeAscii: number = 'a'.charCodeAt(0);
+const zCodeAscii: number = 'z'.charCodeAt(0);
+const nEnglishLetters: number = zCodeAscii - aCodeAscii + 1;
 //HW #29
 function shiftRound(str: string, shift: number): string {
     //TODO
@@ -53,7 +12,7 @@ function shiftRound(str: string, shift: number): string {
     //source letter 'z' will be 'b' if shift ==2
     //example: shiftRound("aabx!", 4) => ("eefb!")
 
-    return "";
+    return cipherDecipher(str, shift, mapperCipher);
 }
 function unshiftRound(str: string, shift: number): string {
     //subtracts "shift" from ASCII codes of lower case letters
@@ -62,5 +21,28 @@ function unshiftRound(str: string, shift: number): string {
     //source letter 'c' will be 'a' if "shift" == 2
     //source letter 'b' will be 'z' if shift ==2
     //example: ushiftRound("eefb!", 4) => ("aabx!")
-    return "";
+    return cipherDecipher(str, shift, mapperDecipher);
 }
+type MapperFunction = (symb: string, shift: number) => string;
+function cipherDecipher(str: string, shift: number,
+    mapperFun: MapperFunction): string {
+       //const arStr: string[] = Array.from(str);
+       const arStr: Array<string> = Array.from(str);
+       const arRes: Array<string> = arStr.map(symb => {
+           let res: string = symb;
+           if (symb <= 'z' && symb >= 'a') {
+               res = mapperFun(symb, shift);
+           }
+           return res;
+       })
+       return arRes.join('');
+    }
+    function mapperCipher(symb: string, shift: number) : string {
+        const actualShift: number = 
+        (symb.charCodeAt(0) - aCodeAscii + shift) % nEnglishLetters;
+        return String.fromCharCode(aCodeAscii + actualShift);
+     }
+     function mapperDecipher(symb: string, shift: number): string {
+        const actualShift: number = (zCodeAscii - symb.charCodeAt(0) + shift) % nEnglishLetters;
+        return String.fromCharCode(zCodeAscii - actualShift);
+     }
